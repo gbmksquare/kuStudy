a2198e84cd1582294c6c08343499e4de07c2f085
BumMo Koo <ksquareatm@gmail.com>
2015-05-30 19:14:32 +0900

    [Kit] Added dynamic framework.

---
+++ kuStudy/kuStudyKit/kuStudyKit.h
@@ -0,0 +1,19 @@
+//
+//  kuStudyKit.h
+//  kuStudyKit
+//
+//  Created by 구범모 on 2015. 5. 30..
+//  Copyright (c) 2015년 gbmKSquare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for kuStudyKit.
+FOUNDATION_EXPORT double kuStudyKitVersionNumber;
+
+//! Project version string for kuStudyKit.
+FOUNDATION_EXPORT const unsigned char kuStudyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kuStudyKit/PublicHeader.h>
+
+
