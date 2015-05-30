fae4d1402782a0d99f3514c9d0c0a675e111d7b2
BumMo Koo <ksquareatm@gmail.com>
2015-05-30 19:05:13 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# kuStudy - Korea University Reading Room App
+
+-----
+
+# Architecture
+
+# Future implementation
+
+# Reference
+
+# Credit
+
+__BumMo Koo__: Develop / Design / Internaionalization
---
+++ kuStudy/kuStudyTests/kuStudyTests.swift
@@ -0,0 +1,36 @@
+//
+//  kuStudyTests.swift
+//  kuStudyTests
+//
+//  Created by 구범모 on 2015. 5. 30..
+//  Copyright (c) 2015년 gbmKSquare. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class kuStudyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
