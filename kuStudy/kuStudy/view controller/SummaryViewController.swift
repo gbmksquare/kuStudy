9dc975cb4794a3fc1d7c3a0cb6565e51d0ead6eb
BumMo Koo <ksquareatm@gmail.com>
2015-05-31 22:35:34 +0900

    [App] Implemented basic interface.

---
+++ kuStudy/kuStudy/view controller/SummaryViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SummaryViewController.swift
+//  kuStudy
+//
+//  Created by 구범모 on 2015. 5. 31..
+//  Copyright (c) 2015년 gbmKSquare. All rights reserved.
+//
+
+import UIKit
+import kuStudyKit
+import SwiftyJSON
+
+class SummaryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var tableView: UITableView!
+    
+    // MARK: View
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: Table view
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("libraryCell", forIndexPath: indexPath) as! UITableViewCell
+        return cell
+    }
+}
