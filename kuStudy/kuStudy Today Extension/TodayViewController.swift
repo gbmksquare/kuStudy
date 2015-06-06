46b31daccbd6781c84ab7e6bb6c179f8d32686ee
BumMo Koo <ksquareatm@gmail.com>
2015-06-06 22:36:00 +0900

    [Today] Implemented basic summary function; Enabled shared container and keychain.

--- kuStudy/kuStudy Today Extension/TodayViewController.swift
+++ kuStudy/kuStudy Today Extension/TodayViewController.swift
@@ -8,27 +8,73 @@
 
 import UIKit
 import NotificationCenter
+import kuStudyKit
+import SwiftyJSON
 
 class TodayViewController: UIViewController, NCWidgetProviding {
-        
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var availableLabel: UILabel!
+    
+    // MARK: Model
+    var summary: Summary?
+    var libraries = [Library]()
+    
+    // MARK: View
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
+        refreshData()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func updateView() {
+        // Summary
+        if let summary = summary {
+            let summaryViewModel = SummaryViewModel(summary: summary)
+            totalLabel.text = summaryViewModel.totalString
+            availableLabel.text = summaryViewModel.availableString
+        }
     }
     
+    // MARK: Action
+    private func refreshData() {
+        kuStudy().setAuthentification(kuStudyAPIAccessId, password: kuStudyAPIAccessPassword)
+        kuStudy().requestSummary { (json, error) -> Void in
+            if let json = json {
+                // Summary
+                let total = json["content"]["total"].intValue
+                let available = json["content"]["available"].intValue
+                self.summary = Summary(total: total, available: available)
+                
+                // Libraries
+                let libraries = json["content"]["libraries"].arrayValue
+                for library in libraries {
+                    let id = library["id"].intValue
+                    let total = library["total"].intValue
+                    let available = library["available"].intValue
+                    let library = Library(id: id, total: total, available: available)
+                    self.libraries.append(library)
+                }
+                self.updateView()
+            } else {
+                // TODO: Handle error
+            }
+        }
+    }
+    
+    // MARK: Widget
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
         // Perform any setup necessary in order to update the view.
 
         // If an error is encountered, use NCUpdateResult.Failed
         // If there's no update required, use NCUpdateResult.NoData
         // If there's an update, use NCUpdateResult.NewData
-
+        
+        refreshData()
         completionHandler(NCUpdateResult.NewData)
     }
     
+    func widgetMarginInsetsForProposedMarginInsets(defaultMarginInsets: UIEdgeInsets) -> UIEdgeInsets {
+        var newMargins = defaultMarginInsets
+        newMargins.bottom = 0
+        return newMargins
+    }
 }
