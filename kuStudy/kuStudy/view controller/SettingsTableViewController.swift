100ca011c2ad2fa4009f54c2d3e721eda9667b11
BumMo Koo <ksquareatm@gmail.com>
2016-09-04 11:23:50 +0900

    Implemented rate on App Store and recommend to a friend.

--- kuStudy/kuStudy/view controller/SettingsTableViewController.swift
+++ kuStudy/kuStudy/view controller/SettingsTableViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AcknowList
+import StoreKit
 
 class SettingsTableViewController: UITableViewController {
     @IBOutlet weak var appIconImageView: UIImageView!
@@ -32,10 +33,40 @@ class SettingsTableViewController: UITableViewController {
 extension SettingsTableViewController {
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         guard let cell = tableView.cellForRowAtIndexPath(indexPath) else { return }
-        if cell.tag == 900 {
+        switch  cell.tag {
+        case 900: // Open source
             let path = NSBundle.mainBundle().pathForResource("Pods-kuStudy-acknowledgements", ofType: "plist")
             let acknowledgementViewController = AcknowListViewController(acknowledgementsPlistPath: path)
             navigationController?.pushViewController(acknowledgementViewController, animated: true)
+        case 998: // Recommend to a friend
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+            let message = "kuStudy.Settings.Recommend".localized() + "\n\nhttps://geo.itunes.apple.com/kr/app/kustudy-golyeodaehaggyo-yeollamsil/id925255895?mt=8&ign-mpt=uo%3D4"
+            let activityVC = UIActivityViewController(activityItems: [message], applicationActivities: nil)
+            presentViewController(activityVC, animated: true, completion: nil)
+        case 999: // App Store review
+            // TODO: On iOS 9, this doesn't allow directly showing review page.
+//            let storeVC = SKStoreProductViewController()
+//            let parameters = [SKStoreProductParameterITunesItemIdentifier: "925255895"]
+//            storeVC.delegate = self
+//            storeVC.loadProductWithParameters(parameters, completionBlock: nil)
+//            presentViewController(storeVC, animated: true, completion: nil)
+            
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+            guard let appUrl = NSURL(string: "itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=925255895&onlyLatestVersion=true&pageNumber=0&sortOrdering=1&type=Purple+Software") else { return }
+            let application = UIApplication.sharedApplication()
+            if application.canOpenURL(appUrl) {
+                application.openURL(appUrl)
+            }
+        default: break
         }
+    }
+}
+
+extension SettingsTableViewController: SKStoreProductViewControllerDelegate {
+    func productViewControllerDidFinish(viewController: SKStoreProductViewController) {
+        if let indexPath = tableView.indexPathForSelectedRow {
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        }
+        viewController.dismissViewControllerAnimated(true, completion: nil)
     }
 }
