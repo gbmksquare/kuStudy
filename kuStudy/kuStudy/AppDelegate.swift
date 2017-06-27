193abb6c51b17ce8bb25fe94efee9cd469c1842b
BumMo Koo <ksquareatm@gmail.com>
2017-06-27 16:40:11 +0900

    [App] Cleaned code in app delegate.

--- kuStudy/kuStudy/AppDelegate.swift
+++ kuStudy/kuStudy/AppDelegate.swift
@@ -20,40 +20,14 @@ import SimulatorStatusMagic
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    // MARK: - Application
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        setupFabric()
-        registerDefaultPreferences()
-        customizeAppearance()
-        setupStatusbarForSnapshot()
-        listenForUserDefaultsDidChange()
-        NetworkActivityIndicatorManager.shared.isEnabled = true
+        setupApplication()
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
     
-    // MARK: Url
+    // MARK: - Url
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
         guard let window = window else { return false }
         
@@ -72,7 +46,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    // MARK: Handoff
+    // MARK: - Handoff
     func application(_ application: UIApplication, willContinueUserActivityWithType userActivityType: String) -> Bool {
         return true
     }
@@ -91,7 +65,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    // MARK: Quick action
+    // MARK: - Quick action
     fileprivate func listenForUserDefaultsDidChange() {
         NotificationCenter.default.addObserver(self, selector: #selector(handleUserDefaultsDidChange(_:)), name: UserDefaults.didChangeNotification, object: nil)
     }
@@ -140,17 +114,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 }
 
-// MARK: Setup
+// MARK: - Setup
 extension AppDelegate {
-    fileprivate func setupFabric() {
-        #if DEBUG
-//            Fabric.with([Crashlytics()])
-        #else
+    fileprivate func setupApplication() {
+        setupFabric()
+        setupDefaultPreferences()
+        setupAppearance()
+        setupStatusbarForSnapshot()
+        listenForUserDefaultsDidChange()
+        NetworkActivityIndicatorManager.shared.isEnabled = true
+    }
+    
+    private func setupFabric() {
+        #if !DEBUG
             Fabric.with([Crashlytics()])
         #endif
     }
     
-    fileprivate func registerDefaultPreferences() {
+    private func setupDefaultPreferences() {
         let defaults = UserDefaults(suiteName: kuStudySharedContainer) ?? UserDefaults.standard
         let libraryOrder = LibraryType.allTypes().map({ $0.rawValue })
         defaults.register(defaults: ["libraryOrder": libraryOrder,
@@ -160,7 +141,7 @@ extension AppDelegate {
         updateQuickActionItems()
     }
     
-    fileprivate func customizeAppearance() {
+    private func setupAppearance() {
         UINavigationBar.appearance().barStyle = .black
         UINavigationBar.appearance().barTintColor = UIColor.theme
         UINavigationBar.appearance().tintColor = UIColor.white
@@ -168,7 +149,7 @@ extension AppDelegate {
         UIBarButtonItem.appearance().tintColor = UIColor.white
     }
     
-    fileprivate func setupStatusbarForSnapshot() {
+    private func setupStatusbarForSnapshot() {
         #if DEBUG
             if ProcessInfo.processInfo.arguments.contains("Snapshot") ? true : false {
                 SDStatusBarManager.sharedInstance().enableOverrides()
