de90ca49db93b599932c991efecfd0ad1c093ff3
BumMo Koo <ksquareatm@gmail.com>
2017-10-14 18:57:24 +0900

    [Kit] Defined two images for main view; Renamed ambiguous property name in Artist class and updated artists preset.

--- kuStudy/kuStudyKit/model/Artist.swift
+++ kuStudy/kuStudyKit/model/Artist.swift
@@ -12,7 +12,7 @@ public struct Artist: Codable, Equatable {
     internal let identifier: String
     public let name: String
     public let association: String
-    public let socialServices: [SocialAccount]
+    public let socialAccounts: [SocialAccount]
     
     public static func ==(lhs: Artist, rhs: Artist) -> Bool {
         return lhs.identifier == rhs.identifier
@@ -25,7 +25,7 @@ internal let artistsPreset = """
         "identifier": "GBM920C5-54F6-42DD-9069-5ED5307D0DEC",
         "name": "구범모",
         "association": "일상다반사",
-        "socialServices":
+        "socialAccounts":
         [
             {
                 "socialService": 0,
@@ -41,7 +41,7 @@ internal let artistsPreset = """
         "identifier": "BGDCCC58-DFCD-4CC2-82A1-8DBC4B4802B0",
         "name": "배경동",
         "association": "일상다반사",
-        "socialServices":
+        "socialAccounts":
         [
             {
                 "socialService": 0,
@@ -53,7 +53,7 @@ internal let artistsPreset = """
         "identifier": "LJY4A5F7-366B-4D31-B9A2-88B323C68561",
         "name": "이준영",
         "association": "일상다반사",
-        "socialServices":
+        "socialAccounts":
         [
             {
                 "socialService": 0,
@@ -65,7 +65,7 @@ internal let artistsPreset = """
         "identifier": "LOVEF079-E23D-43F0-8101-B30A1EE93AA6",
         "name": "전사랑",
         "association": "일상다반사",
-        "socialServices":
+        "socialAccounts":
         [
             {
                 "socialService": 0,
@@ -77,7 +77,7 @@ internal let artistsPreset = """
         "identifier": "RAS57970-CEDD-41A5-BE93-CB9A9C35F6B4",
         "name": "이영준",
         "association": "일상다반사",
-        "socialServices":
+        "socialAccounts":
         [
             {
                 "socialService": 0,
@@ -89,7 +89,7 @@ internal let artistsPreset = """
         "identifier": "SOMBC392-5F74-44C5-B9D0-655A5FCDBF7C",
         "name": "전소미",
         "association": "일상다반사",
-        "socialServices":
+        "socialAccounts":
         [
             {
                 "socialService": 0,
