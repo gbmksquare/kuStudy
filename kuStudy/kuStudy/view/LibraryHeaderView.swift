f9a15f9e9afaec7494859c22918c15b4064a8471
BumMo Koo <ksquareatm@gmail.com>
2017-10-24 23:24:29 +0900

    [App] Implemented new library view controller table header; Implemented SeatsProgress view.

---
+++ kuStudy/kuStudy/view/LibraryHeaderView.swift
@@ -0,0 +1,118 @@
+//
+//  LibraryHeaderView.swift
+//  kuStudy
+//
+//  Created by BumMo Koo on 2017. 10. 24..
+//  Copyright © 2017년 gbmKSquare. All rights reserved.
+//
+
+import UIKit
+
+class LibraryHeaderView: UIView {
+    private lazy var stack = UIStackView()
+    
+    private lazy var titleStack =  UIStackView()
+    private lazy var titleLabel = UILabel()
+    private lazy var subtitleLabel = UILabel()
+    
+    private lazy var dataStack = UIStackView()
+    
+    private lazy var availableStack = UIStackView()
+    private lazy var availableSeatsLabel = UILabel()
+    private lazy var availableLabel = UILabel()
+    
+    private lazy var progressView = SeatsProgressView()
+    
+    private lazy var infoStack = UIStackView()
+    private lazy var timestampLabel = UILabel()
+    private lazy var artistLabel = UILabel()
+    
+    private lazy var separator = UIView()
+    
+    // MARK: - View
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    // MARK: - Setup
+    private func setup() {
+        setupView()
+        setupLayout()
+        titleLabel.text = "Library"
+        subtitleLabel.text = "Library Libertas"
+        availableSeatsLabel.text = "321"
+        availableLabel.text = "Available"
+        timestampLabel.text = "Updated at 12:00"
+        artistLabel.text = "Photography by BumMo Koo"
+    }
+    
+    private func setupView() {
+        stack.axis = .vertical
+        stack.alignment = .fill
+        stack.distribution = .equalSpacing
+        stack.spacing = 16
+        
+        titleStack.axis = .vertical
+        titleStack.alignment = .fill
+        titleStack.distribution = .fillProportionally
+        titleStack.spacing = 4
+        
+        dataStack.axis = .horizontal
+        dataStack.alignment = .lastBaseline
+        dataStack.distribution = .equalSpacing
+        dataStack.spacing = 8
+        
+        availableStack.axis = .vertical
+        availableStack.alignment = .fill
+        availableStack.distribution = .fillProportionally
+        availableStack.spacing = 2
+        
+        infoStack.axis = .horizontal
+        infoStack.alignment = .fill
+        infoStack.distribution = .equalSpacing
+        infoStack.spacing = 4
+        
+        titleLabel.font = UIFont.systemFont(ofSize: 33, weight: .bold)
+        subtitleLabel.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        availableSeatsLabel.font = UIFont.systemFont(ofSize: 36, weight: .regular)
+        availableLabel.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        timestampLabel.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        artistLabel.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+        
+        subtitleLabel.textColor = .darkGray
+        timestampLabel.textColor = .lightGray
+        artistLabel.textColor = .lightGray
+        separator.backgroundColor = #colorLiteral(red: 0.8392109871, green: 0.8391088247, blue: 0.8563356996, alpha: 1)
+    }
+    
+    private func setupLayout() {
+        [stack, separator].forEach { addSubview($0) }
+        [titleStack, dataStack, infoStack].forEach { stack.addArrangedSubview($0) }
+        [titleLabel, subtitleLabel].forEach { titleStack.addArrangedSubview($0) }
+        [availableStack, progressView].forEach { dataStack.addArrangedSubview($0) }
+        [availableSeatsLabel, availableLabel].forEach { availableStack.addArrangedSubview($0) }
+        [timestampLabel, artistLabel].forEach { infoStack.addArrangedSubview($0) }
+        
+        stack.snp.makeConstraints { (make) in
+            make.top.equalTo(snp.topMargin)
+            make.leading.equalTo(readableContentGuide).inset(8)
+            make.trailing.equalTo(readableContentGuide).inset(8)
+            make.bottom.equalTo(snp.bottomMargin)
+        }
+        progressView.snp.makeConstraints { (make) in
+            make.width.equalTo(dataStack).multipliedBy(0.6)
+        }
+        separator.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.height.equalTo(0.3)
+        }
+    }
+}
---
+++ kuStudy/kuStudy/view/SeatsProgressView.swift
@@ -0,0 +1,89 @@
+//
+//  SeatsProgressView.swift
+//  kuStudy
+//
+//  Created by BumMo Koo on 2017. 10. 24..
+//  Copyright © 2017년 gbmKSquare. All rights reserved.
+//
+
+import UIKit
+
+class SeatsProgressView: UIView {
+    private lazy var occupiedStack = UIStackView()
+    private lazy var occupiedSeatsLabel = UILabel()
+    private lazy var occupiedLabel = UILabel()
+    
+    private lazy var totalStack = UIStackView()
+    private lazy var totalSeatsLabel = UILabel()
+    private lazy var totalLabel = UILabel()
+    
+    private lazy var progressView = UIProgressView()
+    
+    // MARK: - Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    // MARK: - Setup
+    private func setup() {
+        setupView()
+        setupLayout()
+        
+        occupiedSeatsLabel.text = "123"
+        occupiedLabel.text = "Occupied"
+        totalSeatsLabel.text = "1234"
+        totalLabel.text = "Total"
+        progressView.progress = 0.65
+    }
+    
+    private func setupView() {
+        [occupiedStack, totalStack].forEach {
+            $0.axis = .vertical
+            $0.distribution = .fillProportionally
+            $0.spacing = 1
+        }
+        occupiedStack.alignment = .leading
+        totalStack.alignment = .trailing
+        
+        [occupiedSeatsLabel, totalSeatsLabel].forEach {
+            $0.textColor = .darkGray
+            $0.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+        }
+        
+        [occupiedLabel, totalLabel].forEach {
+            $0.textColor = .darkGray
+            $0.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        }
+    }
+    
+    private func setupLayout() {
+        [occupiedStack, totalStack, progressView].forEach { addSubview($0) }
+        [occupiedSeatsLabel, occupiedLabel].forEach { occupiedStack.addArrangedSubview($0) }
+        [totalSeatsLabel, totalLabel].forEach { totalStack.addArrangedSubview($0) }
+        
+        occupiedStack.snp.makeConstraints { (make) in
+            make.top.greaterThanOrEqualToSuperview()
+            make.leading.equalToSuperview()
+            make.bottom.equalTo(progressView.snp.top).offset(-8)
+        }
+        
+        totalStack.snp.makeConstraints { (make) in
+            make.top.equalTo(occupiedStack)
+            make.trailing.equalToSuperview()
+            make.bottom.equalTo(occupiedStack)
+        }
+        
+        progressView.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.height.equalTo(4)
+        }
+    }
+}
