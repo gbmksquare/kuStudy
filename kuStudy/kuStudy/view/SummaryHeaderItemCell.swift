6811a5e002e9b6d3483562c4a4007614324041eb
BumMo Koo <ksquareatm@gmail.com>
2018-06-23 21:49:49 +0900

    [App] Added background color to summary header item cell.

--- kuStudy/kuStudy/view/SummaryHeaderItemCell.swift
+++ kuStudy/kuStudy/view/SummaryHeaderItemCell.swift
@@ -27,7 +27,7 @@ class SummaryHeaderItemCell: UICollectionViewCell {
     }
     
     private func setup() {
-        containerView.backgroundColor = .white
+        containerView.backgroundColor = #colorLiteral(red: 0.9803921569, green: 0.9803921569, blue: 0.9803921569, alpha: 1)
         containerView.layer.cornerRadius = 9
         containerView.layer.shadowColor = UIColor.black.cgColor
         containerView.layer.shadowOffset = CGSize(width: 0, height: 0)
