2f4c435acb7e890602da5b1f2c2de976cadb5f5d
BumMo Koo <ksquareatm@gmail.com>
2018-10-15 17:47:57 +0900

    [App] Changed UIColor extension.

--- kuStudy/kuStudyKit/extension/UIColor.swift
+++ kuStudy/kuStudyKit/extension/UIColor.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 public extension UIColor {
-    static var error: UIColor { return #colorLiteral(red: 0.9921568627, green: 0.3764705882, blue: 0.3411764706, alpha: 1) }
-    static var warning: UIColor { return #colorLiteral(red: 0.9799304605, green: 0.5882352941, blue: 0, alpha: 1) }
-    static var warningLight: UIColor { return #colorLiteral(red: 0.9995546937, green: 0.7494820952, blue: 0.1818162799, alpha: 1) }
-    static var confirm: UIColor { return #colorLiteral(red: 0.1646617353, green: 0.7905144691, blue: 0.2539618611, alpha: 1) }
+    static let error: UIColor = #colorLiteral(red: 0.9921568627, green: 0.3764705882, blue: 0.3411764706, alpha: 1)
+    static let warning: UIColor = #colorLiteral(red: 0.9799304605, green: 0.5882352941, blue: 0, alpha: 1)
+    static let warningLight: UIColor = #colorLiteral(red: 0.9995546937, green: 0.7494820952, blue: 0.1818162799, alpha: 1)
+    static let confirm: UIColor = #colorLiteral(red: 0.1646617353, green: 0.7905144691, blue: 0.2539618611, alpha: 1)
 }
